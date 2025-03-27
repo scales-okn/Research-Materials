9f294bbedf91b8c14ee494a3121b3203cdca7e6a
scottgdaniel <scottgdaniel@gmail.com>
2025-03-27 14:48:42 -0500

    support update

--- code/support/core.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-def std_path(fpath):
-    ''' Standardise a filepath, returns a Path object'''
-    if type(fpath) is str:
-        fpath = Path(fpath.replace('\\','/'))
-    return fpath
